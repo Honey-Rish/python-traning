f1b3f022652f3fb7b84fe394adec448c744982ed
CHARITHA <90490913+TOBEKOWN@users.noreply.github.com>
2022-05-23 22:57:03 +0530

    Its my new assignment's of python programming basics

---
+++ skassign2.py
@@ -0,0 +1,16 @@
+#To read the marks of three Subjects and Display the Total, Avg and Class
+a = int(input("Enter the marks of first subject: "))
+b = int(input("Enter the marks of second subject: "))
+total = a+b
+avg = total/2
+print("Total marks: ",total)
+print("Average marks: ",avg)
+
+#accpet the number and display square and cude of the given number
+
+a = int(input("Enter the number: "))
+square=a*a
+cube =a*a*a
+print("square of a number: ",square)
+print("cube of a number: ",cube)
+
